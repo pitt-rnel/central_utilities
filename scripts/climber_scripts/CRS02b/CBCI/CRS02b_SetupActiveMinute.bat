cd9f839c74b2755b90e3bf7ad92b77d1bd61360b
Jeff Weiss <jmw182@pitt.edu>
2023-05-01 15:11:12 -0400

    Added recording scripts for CBCI

---
+++ scripts/climber_scripts/CRS02b/CBCI/CRS02b_SetupActiveMinute.bat
@@ -0,0 +1,27 @@
+@echo off
+
+:: Set subject
+set SUBJECT_ID=CRS02b
+:: Set pedestal ID and cbInst ID params for 2 pedestals
+set PED1=\
+set CB1=0
+set FIRST="active_minute"
+set LAST="none"
+set SUBFOLDER=ActiveMinute
+
+:: Enable raw
+call ..\..\base_scripts\CRS_CentralAutoRaw.bat %CB1% 1
+
+:: wait 2 seconds
+@timeout /t 2 /nobreak
+
+:: Fill in info
+call ..\..\base_scripts\CRS_CentralAutoRecordInfo.bat %CB1% %SUBJECT_ID% %PED1% %FIRST% %LAST% %SUBFOLDER%
+
+:: wait for input to disable raw data
+@echo.
+@echo "Press any key to disable raw data (after recordings are complete)"
+@PAUSE
+
+:: Disable raw
+call ..\..\base_scripts\CRS_CentralAutoRaw.bat %CB1% 0
---
+++ scripts/climber_scripts/CRS07/CBCI/CRS07_SetupQuietMinute_Post.bat
@@ -0,0 +1,27 @@
+@echo off
+
+:: Set subject
+set SUBJECT_ID=CRS07
+:: Set pedestal ID and cbInst ID params for 2 pedestals
+set PED1=\
+set CB1=0
+set FIRST="quiet_minute"
+set LAST="none"
+set SUBFOLDER=QuietMinute_Post
+
+:: Enable raw
+call ..\..\base_scripts\CRS_CentralAutoRaw.bat %CB1% 1
+
+:: wait 2 seconds
+@timeout /t 2 /nobreak
+
+:: Fill in info
+call ..\..\base_scripts\CRS_CentralAutoRecordInfo.bat %CB1% %SUBJECT_ID% %PED1% %FIRST% %LAST% %SUBFOLDER%
+
+:: wait for input to disable raw data
+@echo.
+@echo "Press any key to disable raw data (after recordings are complete)"
+@PAUSE
+
+:: Disable raw
+call ..\..\base_scripts\CRS_CentralAutoRaw.bat %CB1% 0
