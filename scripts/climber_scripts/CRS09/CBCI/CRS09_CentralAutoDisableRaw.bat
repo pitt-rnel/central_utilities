67e433bf1c4060787439f79d9616d9093e528196
Pitt BCI Rig <bcigit@pitt.edu>
2025-08-25 12:15:51 -0400

    initial commit of CRS09 scripts

---
+++ scripts/climber_scripts/CRS09/CBCI/CRS09_CentralAutoDisableRaw.bat
@@ -0,0 +1,15 @@
+:: Script to enable raw data in Central
+:: First input is cbInst
+:: Second input is enableFlag (0 to disable raw, 1 to enable raw)
+@echo off
+
+:: Set Input Values
+SET cbInst=0
+SET enableFlag=0
+
+:: bin dir
+SET bin=%~dp0\..\..\..\..\bin
+
+@echo on
+::Run Central Auto Raw
+%bin%\CentralAutoRaw.exe %cbInst% %enableFlag%
